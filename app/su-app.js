eca80df4e0a79800b9a953fe113364d3a3db08f8
engladian <simon.franklin2@cigna.com>
2015-09-19 19:53:53 +1200

    Competed

---
+++ app/su-app.js
@@ -0,0 +1,19 @@
+﻿'use strict';
+var appModule = angular.module('suApp',[]);
+
+appModule.controller('loginController', function() {});
+
+appModule.directive('optIn', function() {
+    return {
+        restrict: 'E',
+        transclude: true,
+        template: '<div ng-transclude></div><login></login>'
+    }
+});
+
+appModule.directive('login', function() {
+    return {
+        restrict: 'E',
+        templateUrl: "./sign-up/sign-up-form.html"
+    }
+});
